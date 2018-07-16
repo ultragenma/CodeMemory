6db0273e0608e34b775fe6bad33c66facee75782
Chockie Twickie <chockietwickie@gmail.com>
2018-07-16 13:40:11 +0900

    Add GameChoice, Game

---
+++ app/src/main/java/jp/co/ultragenma/codememory/Game.java
@@ -0,0 +1,11 @@
+package jp.co.ultragenma.codememory;
+
+import android.view.View;
+
+public class Game {
+
+    public Game(View view) {
+
+    }
+
+}
--- app/src/main/java/jp/co/ultragenma/codememory/MainActivity.java
+++ app/src/main/java/jp/co/ultragenma/codememory/MainActivity.java
@@ -4,9 +4,14 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 import android.os.Bundle;
+import android.print.PrinterId;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        setScreenMain();    // set main activity
 
         // Load an ad into the AdMob banner view.
         AdView adView = (AdView) findViewById(R.id.adView);
@@ -27,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         adView.loadAd(adRequest);
 
         // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
-        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
     }
 
 
@@ -46,10 +53,49 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
-            return true;
+            setScreenGameChoice();      // set game choice screen
+            return true;gi
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setScreenMain() {
+        setContentView(R.layout.activity_main);
+
+        ImageButton btn = findViewById(R.id.app_icon);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setScreenGameChoice();      // set game choice screen
+            }
+        });
+    }
+
+    private void setScreenGameChoice() {
+        setContentView(R.layout.layout_game_choice);
+
+        Button btn_all = findViewById(R.id.tokyo_all);
+        btn_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setScreenGame();
+            }
+        });
+    }
+
+    private void setScreenGame() {
+        setContentView(R.layout.layout_game);
+
+        final TextView answer = findViewById(R.id.field_answer);
+        answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answer.setVisibility(View.INVISIBLE);
+                // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
+                Toast.makeText(view.getContext(), TOAST_TEXT, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
